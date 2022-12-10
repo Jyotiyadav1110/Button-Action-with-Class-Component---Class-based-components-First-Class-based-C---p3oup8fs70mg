1e8726630855daab48a0449fd3f5482af8fd0b49
Jyoti Yadav <82959912+Jyotiyadav1110@users.noreply.github.com>
2022-12-11 00:51:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
+
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			showPara : false
+		}
 	};
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={()=>{this.setState({showPara:true})}}>Show Para</button>
+				{this.state.showPara && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
@@ -17,4 +23,3 @@ class App extends Component {
 
 
 export default App;
-
